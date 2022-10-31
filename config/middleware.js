5f110367ef2abcc76298eb20b49c4b332bd3d6cf
Sahil Arora <sahil2398@yahoo.com>
2022-10-31 22:56:04 +0530

    heroku and mongodb related

--- config/middleware.js
+++ config/middleware.js
@@ -12,7 +12,6 @@ exports.verifyToken = async (req, res, next) => {
   
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
       token = req.headers.authorization.split(" ")[1];
-      // console.log("TOKEN : "+token);
       req.token = token;
     }
 
--- config/mongoose.js
+++ config/mongoose.js
@@ -3,9 +3,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-let DB_URL = process.env. DB_URL
+let DB_URL = process.env.DB_URL
+// console.log(DB_URL)
 
-mongoose.connect( DB_URL || 'mongodb://localhost/hospital_API', { useNewUrlParser:true ,useUnifiedTopology: true });
+mongoose.connect( DB_URL, { useNewUrlParser:true ,useUnifiedTopology: true });
+//mongoose.connect( DB_URL || 'mongodb://localhost/hospital_API', { useNewUrlParser:true ,useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB DB"));
